ee71ffc31d63976607d22f35b903699f95b7001e
MatiasGonzalez <matiaspch@gmail.com>
2020-05-09 00:22:11 -0300

    Se agrega nombre de usuario y contraseña

--- models/estudiante.js
+++ models/estudiante.js
@@ -4,7 +4,9 @@ module.exports = (sequelize, DataTypes) => {
     nombre: DataTypes.STRING,
     apellido: DataTypes.STRING,
     email: DataTypes.STRING,
-    edad: DataTypes.INTEGER
+    edad: DataTypes.INTEGER,
+    usuario:DataTypes.STRING,
+    password:DataTypes.STRING
   }, {});
   Estudiante.associate = function(models) {
     // associations can be defined here
