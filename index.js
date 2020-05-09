dc688e5d30a3f0990875938c9382d482cb3c7fcc
LuisObesto <luis_obesto@hotmail.com>
2020-05-08 23:55:57 -0300

    Consulta a la bd, mediante la carpeta modelsv2 con la funcion userModels.js e instalacion de util "npm i util" para hacer consultas sincronicas

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 'use strict'
 
 var mysql = require('mysql');
+const util = require('util');
 
-var conexion = mysql.createConnection({
+var pool = mysql.createConnection({
     host: 'localhost',
     user : 'root',
-    password : null,
-    database : 'proyecto_facultad'
+    password : 'linkinpark123',
+    database : 'proyecto_facultad_foro'
 });
-conexion.connect(function(error){
-    if(error) throw error;
-    console.log("connected");
-} );
+//Convertir el objeto pool en una peticion sincronica
+//pool.query() gestiona las peticiones (pool referencia conexion / query metodo que permite hacer consultas o transaccion de la base de datos)
 
-const models = require('./models');
-const estudiante = models.Estudiante;
-const estudianteFacultad = models.estudianteFacultad;
-const facultad = models.facultad;
-estudiante.create({
-    nombre:"Estudiante1",
-    apellido:"EstudianteApellido",
-    email:"estu@estu.com"
-}).then((newEstudiante) => {
-    console.log(newEstudiante.get())
-}).catch((err)=>{
-    console.log("Error al crear estudiante : ",err);
-})
+pool.query = util.promisify(pool.query);//Transforma pool.query en una funcion sincronica y llamarla pool.query (el mismo)
+module.exports = pool; //Exportamos la referencia de la conexion, que sea publica para cualquier ruta
+
+
+
+
+
+// const models = require('./models/estudiante');
+// const estudiante = models.estudiante;
+// const estudianteFacultad = models.estudianteFacultad;
+// const facultad = models.facultad;
+// estudiante.create({
+//     nombre:"Estudiante1",
+//     apellido:"EstudianteApellido",
+//     email:"estu@estu.com"
+// }).then((newEstudiante) => {
+//     console.log(newEstudiante.get())
+// }).catch((err)=>{
+//     console.log("Error al crear estudiante : ",err);
+// })
 // facultad.create({
 //     nombre:"facultad1",
 //     direccion : "pepe123",
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require('../modelsv2/userModel');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/new', async(req,res,next)=>{
+
+  try {
+    
+    let object ={
+      nombre: req.body.nombre,
+      apellido: req.body.apellido,
+      email : req.body.email,
+      edad : req.body.edad,
+     
+    }
+
+    console.log(req.body);
+    await userModel.createrUser(object);
+
+    res.json({status : true, message : 'Usuario registrado correctamente'})
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({status : false, message :error});
+  }
+
+})
 
 module.exports = router;
