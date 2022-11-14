04e68ce11b5539a232b12c3b230ed136683ae1d5
AlanGMF <96092963+AlanGMF@users.noreply.github.com>
2022-11-14 20:04:35 -0300

    error!
    add

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use('/estudianteFacu',estudFacu);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
+// error!
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
